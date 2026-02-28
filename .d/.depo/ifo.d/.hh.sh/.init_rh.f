8467ef922acd9da6c7c6058bef3ce62fe798c807
legioner9 <legioner9@inbox.ru>
2026-02-28 18:36:40 +0700

    <>

--- .d/.depo/ifo.d/.hh.sh/.init_rh.f
+++ .d/.depo/ifo.d/.hh.sh/.init_rh.f
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-scp -v $HOME/aer_foe/.d/.depo/ifo.d/.rh.sh/.init_ubuntu.f root@185.228.234.60/root/
+scp -l 1000 $HOME/aer_foe/.d/.depo/ifo.d/.rh.sh/.init_ubuntu.f root@185.228.234.60:/root/
