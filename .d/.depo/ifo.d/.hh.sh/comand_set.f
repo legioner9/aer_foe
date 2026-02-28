d0e44e39cecb6fb64b97f304da7b30190dae37cf
legioner9 <legioner9@inbox.ru>
2026-02-28 19:15:41 +0700

    <>

---
+++ .d/.depo/ifo.d/.hh.sh/comand_set.f
@@ -0,0 +1,20 @@
+# rh (remote_host)
+
+    # sudo systemctl status ssh
+
+
+
+# hh (home_host)
+
+    ssh root@185.228.234.60 "df -h"
+                            ". .init_ubuntu.f"
+                            "ls -A /root/"
+
+## удленная точка монтирования `sshfs пользователь@удаленный_хост:/удаленный/путь /локальная/точка/монтирования`
+
+    hh#sshfs root@185.228.234.60:/root/hh.fs $HOME/aer_foe/.d/.sshfs/gb.sshfs
+
+
+
+
+
