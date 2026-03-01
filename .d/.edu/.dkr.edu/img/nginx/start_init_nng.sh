c4f4357800d4842de9f73425a56fbac4ce65e7f2
[alt]legioner9 <legioner9@inbox.ru>
2026-03-01 23:08:42 +0700

    <>

--- .d/.edu/.dkr.edu/img/nginx/start_init_nng.sh
+++ .d/.edu/.dkr.edu/img/nginx/start_init_nng.sh
@@ -5,4 +5,5 @@ stl_env_nng_html=/usr/share/nginx/html
 dkc stop nng
 dkr rm nng
 
-dkr run -d --name nng -p 8080:80 -v /c/Users/ProNout/aer_foe/.d/.edu/.dkr.edu/www:$stl_env_nng_html nginx
+dkr run -d --name nng -p 8880:80 -v $HOME/aer_foe/.d/.edu/.dkr.edu/www:$stl_env_nng_html nginx
+# dkr run -d --name nng -p 8080:80 nginx
