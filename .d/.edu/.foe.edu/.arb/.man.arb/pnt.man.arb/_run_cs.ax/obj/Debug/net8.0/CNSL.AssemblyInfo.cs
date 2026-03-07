c5fcfb48562515f642fca3cff050c4e67cf31de6
legioner9 <legioner9@inbox.ru>
2026-03-07 12:12:39 +0700

    <>

--- .d/.edu/.foe.edu/.arb/.man.arb/pnt.man.arb/_run_cs.ax/obj/Debug/net8.0/CNSL.AssemblyInfo.cs
+++ .d/.edu/.foe.edu/.arb/.man.arb/pnt.man.arb/_run_cs.ax/obj/Debug/net8.0/CNSL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNSL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f095c2ab1d3609bd06013d07c40aa12688beec43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f3c9de180277f915aad2f07509d19bcffb3aef")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNSL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNSL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
