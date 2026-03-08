609e3a50007aa9e9a06d445f4a1a67b21bb859ae
legioner9 <legioner9@inbox.ru>
2026-03-08 17:00:41 +0700

    <>

--- .d/.edu/srs_mll.edu/.exa.lstd/001_i1/.sh/start_cmp_build.sh
+++ .d/.edu/srs_mll.edu/.exa.lstd/001_i1/.sh/start_cmp_build.sh
@@ -2,8 +2,10 @@
 
 bkpth=$HOME/aer_foe/.d/.edu/srs_mll.edu/.exa.lstd
 nmdir="001_i1"
-wdir=$HOME/aer_foe/.d/.edu/srs_mll.edu/.exa.lstd/$nmdir/wkrdr
+wdir=$HOME/aer_foe/.d/.edu/srs_mll.edu/.exa.lstd/$nmdir/wrkdr
 
 cd $bkpth/$nmdir || return 1
+
+pwd
 
 docker-compose build 
