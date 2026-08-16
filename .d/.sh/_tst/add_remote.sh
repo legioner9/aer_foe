356e6564f6e94a2685d61c5099657975bb89fdc6
legioner9 <legioner9@inbox.ru>
2026-08-16 16:37:16 +0700

    <>

---
+++ .d/.sh/_tst/add_remote.sh
@@ -0,0 +1,3 @@
+			git remote add gh git@github.com:legioner9/edu_dta_001.git
+			git remote add gf git@gitflic.ru:legioner9/edu_dta_001.git
+			git remote add alt ssh://forgejo@altlinux.space/legioner9/edu_dta_001.git
