34869f811dc89bb0f22c479f6f9322f61bbbf5a5
legioner9 <legioner9@inbox.ru>
2026-08-21 07:31:06 +0700

    <>

--- .d/.edu/puml/1/start_2.sh
+++ .d/.edu/puml/1/start_2.sh
@@ -4,6 +4,6 @@ dir=~/aer_foe/.d/.edu/puml/1
 
 cd $dir
 
-java -jar ~/aer_foe/.d/.cdn.ax/java/plantuml.jar abs.puml 
+cat init1.puml | java -jar ~/aer_foe/.d/.cdn.ax/java/plantuml.jar -pipe > res/res.png
 # java -jar ~/aer_foe/.d/.cdn.ax/java/plantuml.jar abs.puml -o dew.png.d/34.png
 # java -cp /home/st/aer_foe/.d/.cdn.ax/java -jar ~/aer_foe/.d/.cdn.ax/java/plantuml.jar -h
